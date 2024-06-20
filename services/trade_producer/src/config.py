412f7e9d2e808c2ee63eb52a910d9644538d8da8
max-mcd <max-mcd@outlook.com>
2024-06-20 12:10:49 -0700

    Extract config parameters as environment variables, add readme
    Define KAFKA_BROKER_ADDRESS as an environment variable so the Trade Producer
    can either be run locally or within a docker container.

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,3 +1,9 @@
-kafka_broker_address='redpanda-0:9092'
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Load environment variables from .env file for local run
+load_dotenv(find_dotenv())
+
+kafka_broker_address=os.environ['KAFKA_BROKER_ADDRESS']
 kafka_topic_name='trade'
 product_id='BTC/USD'
